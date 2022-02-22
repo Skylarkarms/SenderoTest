ca99bb2e3305216bbc342dedc1f31c0f1523fb33
Juan Andrade <juanandrade_20@hotmail.com>
2022-02-22 15:45:43 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import sendero.Gates;
+import sendero.Merge;
+
+public class Main {
+    public static void main(String[] args) {
+        Gates.In<String> hello = new Gates.In<>("Hello");
+        Gates.In<String> world = new Gates.In<>("World");
+        Merge<String[]> helloWorld = new Merge<>(new String[2], strings -> {
+            boolean hasNulls = false;
+            for (String s:strings
+                 ) {
+                if (s == null) {
+                    hasNulls = true;
+                    break;
+                }
+            }
+            return hasNulls;
+        });
+        helloWorld.from(hello, updater -> helloString -> {
+            updater.update(strings -> {
+                String[] newRes = strings.clone();
+                newRes[0] = helloString;
+                return newRes;
+            });
+        });
+        helloWorld.from(world, updater -> worldString -> {
+            updater.update(strings -> {
+                String[] newRes = strings.clone();
+                newRes[1] = worldString;
+                return newRes;
+            });
+        });
+
+        Gates.Out.Single<String> result = helloWorld.forkMap(
+                strings -> strings[0] + " " + strings[1]
+        ).out(Gates.Out.Single.class);
+
+        result.register(
+                s -> {
+                    System.out.println(s);
+                    result.unregister();
+                }
+        );
+    }
+}
